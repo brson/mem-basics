c381042497e8a7faa1d058b5868015f246151f2c
Brian Anderson <andersrb@gmail.com>
2020-05-21 04:27:35 +0000

    .

--- src/memcpy/memcpy.rs
+++ src/memcpy/memcpy.rs
@@ -1,3 +1,12 @@
+#![no_std]
 #![allow(unused)]
 
 use cfg_if::cfg_if;
+
+pub mod memcpy_trivial;
+
+pub type Memcpy = unsafe fn(dst: *mut u8, src: *mut u8, bytes: usize);
+
+pub static ALL_MEMCPYS: &[Memcpy] = &[
+    memcpy_trivial::memcpy,
+];
---
+++ src/memcpy/memcpy_trivial.rs
@@ -0,0 +1,3 @@
+pub unsafe fn memcpy(dst: *mut u8, src: *mut u8, bytes: usize) {
+    panic!()
+}
